430b1a5118d5f28eda7443f5b14557ad31a7e275
enuggetry <github@meyao.net>
2017-06-13 19:57:19 -0700

    comments

--- ex-basic.js
+++ ex-basic.js
@@ -2,32 +2,36 @@ var promise = require('bluebird');
 var fs = promise.promisifyAll(require("fs"));
 var request = promise.promisifyAll(require("request"));
 
-fs.readFileAsync("myfile.json", "utf8").then(function(data) {
-
-});
-
-
 var r1, r2, r3;
 fs.readFileAsync("myfile.json", "utf8").then(function(data) {
-     r1 = data;
-     
-     var url = 'https://rawgit.com/enuggetry/promise-examples/master/myfile.json';
-     return request(url);
+    r1 = data;
+
+    // just unplug your network to make this fail.
+
+    var url = 'https://rawgit.com/enuggetry/promise-examples/master/myfile.json';
+    return request.getAsync({url:url,json:true});
 }).then(function(data2) {
-     r2 = result2;
-     
-     
-     return fs.readFileAsync("myfile_1.json", "utf8");
+    r2 = data2.body;
+
+    console.log("statusCode",data2.statusCode,"statusMessage",data2.statusMessage);
+
+    // delete this file to make this fail
+
+    return fs.readFileAsync("myfile_1.json", "utf8");
 }).then(function(data3) {
-     r3 = data3;
+    r3 = data3;
+
+    // can access r1 or r2 or r3
+    console.log(">>> r1",r1);
+    console.log(">>> r2",r2);
+    console.log(">>> r3",r3);
      
-     // can access r1 or r2 or r3
-     console.log(">>> r1",r1);
-     console.log(">>> r2",r2);
-     console.log(">>> r3",r3);
      
-     
-}).catch(function(err){
+}).catch(function(err){                 // catch all errors
     //error handling logic
     console.log('error',err);
+    
+    console.log("r1",typeof r1);
+    console.log("r2",typeof r2);
+    console.log("r3",typeof r3);
 });
